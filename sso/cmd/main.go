1aa17cc145e6000defc4334495bc5cda83f4f9af
artyomstank <art3mii411@gmail.com>
2025-05-17 23:17:40 +0300

    toLen -> toKen, main.go +logs

--- sso/cmd/main.go
+++ sso/cmd/main.go
@@ -2,11 +2,51 @@ package main
 
 import (
 	"D/project/1_trial/sso/internal/config"
-	"fmt"
+	// "fmt"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application",
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+		slog.Int("port", cfg.GRPC.Port))
+
+	log.Debug("debug msg")
+
+	log.Error("err msg")
+
+	log.Warn("warn msg")
+
+}
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+
+	}
+	return log
 }
--- sso/internal/config/config.go
+++ sso/internal/config/config.go
@@ -11,7 +11,7 @@ import (
 type Config struct {
 	Env         string        `yaml:"env" env-default:"local"`
 	StoragePath string        `yaml:"storage_path" enc-required:"true"`
-	TokenTTL    time.Duration `yaml:"tolen_ttl" env-required:"true"`
+	TokenTTL    time.Duration `yaml:"token_ttl" env-required:"true"`
 	GRPC        GRPCConfig    `yaml:"grpc"`
 }
 
